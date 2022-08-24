535e2853f9e8b2d4906cedfbada99b032c1ca953
elda-mahaindra <faith030@gmail.com>
2022-08-24 19:09:58 +0700

    root query

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,3 +1,20 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLSchema } from "graphql";
 
 import BookType from "./book";
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => {
+        // code to get data from db / other source
+      },
+    },
+  },
+});
+
+export default new GraphQLSchema({
+  query: RootQuery,
+});
