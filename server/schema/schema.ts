1fdaf9d2f5a91f2131b64d900aea995fe1d11236
elda-mahaindra <faith030@gmail.com>
2022-08-24 19:22:07 +0700

    the resolve function

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -2,6 +2,13 @@ import { GraphQLObjectType, GraphQLString, GraphQLSchema } from "graphql";
 
 import BookType from "./book";
 
+// dummy data
+const books = [
+  { name: "Name of The Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
@@ -10,6 +17,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve: (parent, args) => {
         // code to get data from db / other source
+        return books.find((book) => book.id === args.id);
       },
     },
   },
