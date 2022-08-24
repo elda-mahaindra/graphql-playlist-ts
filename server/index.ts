a1f59289819893783b804ae857f0b2d7f173baf5
elda-mahaindra <faith030@gmail.com>
2022-08-24 17:42:44 +0700

    setting up graphql

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
+
+// app.use("/graphql", graphqlHTTP({}));
 
 app.listen(4000, () => {
   console.log("Now listening for request on port 4000");
